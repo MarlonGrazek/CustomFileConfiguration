051a1db5c5836966baaa24ad989a1e9ad2b1cd82
Marlon Grazek <business.marlongrazek@gmail.com>
2022-09-02 18:38:46 +0200

    Initial commit

---
+++ src/main/java/com/marlongrazek/customfileconfiguration/CFC.java
@@ -0,0 +1,58 @@
+package com.marlongrazek.customfileconfiguration;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
+public final class CFC extends YamlConfiguration {
+
+    private final Plugin plugin;
+    private final File file;
+
+    public CFC(String name, Plugin plugin) throws IOException, InvalidConfigurationException {
+
+        this.plugin = plugin;
+        file = new File(plugin.getDataFolder(), name.endsWith(".yml") ? name : name + ".yml");
+
+        loadConfig();
+    }
+
+    public void save() {
+        try {
+            save(file);
+        } catch (IOException e) {
+            plugin.getLogger().log(Level.SEVERE, "There was an error while saving the " + file.getName() + " configuration!", e);
+        }
+    }
+
+    public void reload() {
+        try {
+            loadConfig();
+        } catch (IOException e) {
+            plugin.getLogger().log(Level.SEVERE, "There was an error while creating the " + file.getName() + " configuration file!", e);
+        } catch (InvalidConfigurationException e) {
+            plugin.getLogger().log(Level.SEVERE, "The configuration " + file.getName() + " is invalid!", e);
+        }
+    }
+
+    private void loadConfig() throws IOException, InvalidConfigurationException {
+        if (!plugin.getDataFolder().exists())
+            plugin.getDataFolder().mkdir();
+
+        if (!file.exists()) {
+            try {
+                // Save the config file if found inside the jar
+                plugin.saveResource(file.getName(), false);
+            } catch (IllegalArgumentException e) {
+                // File not found in resources, creating a blank one
+                file.createNewFile();
+            }
+        }
+
+        load(file);
+    }
+}
